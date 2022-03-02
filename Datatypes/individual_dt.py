56625a9b258e8949c6473e0d892a03a502d0fed8
vidyagowda159 <vidyagowda005@gmail.com>
2022-03-02 10:27:01 +0530

    commiting datatypes and basics files

---
+++ Datatypes/individual_dt.py
@@ -0,0 +1,9 @@
+""" Integers """
+a = 10
+b = int(86)
+
+# default value
+c = int()
+d = 0
+print(a, b, c)
+
---
+++ basics.py
@@ -0,0 +1,17 @@
+# print("hello", end=" ")
+# print("hai")
+
+
+# print("hai", "hello", "python", sep="_")
+
+# a = input("enter the value of a: ")
+# print(a)
+
+""" Activity """
+
+# a = "Ram"
+# b = 10
+# print("My name is ", a, sep="&&", end=" ")
+# print("I am", b, "years old", sep="&&")
+
+# print(sep="@", "hai", "hello")  # SyntaxError
