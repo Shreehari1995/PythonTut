045b642eb4b3d3fe9a251def197590aec17ae574
vidyagowda159 <vidyagowda005@gmail.com>
2022-03-21 15:57:21 +0530

    file_handling

---
+++ file_handling/basics_os.py
@@ -0,0 +1,40 @@
+import os
+
+# current working directory
+print(os.getcwd())
+
+# path = r"C:\Users\Vidyashree M C\PycharmProjects\Alpha4\Datatypes"
+# changing the directory
+# os.chdir(path)
+# print(os.getcwd())
+
+# creating folder
+# os.mkdir("sample")
+
+# delete folder
+# os.rmdir("sample")
+
+# path = r"C:\Users\Vidyashree M C\PycharmProjects\Alpha4"
+
+# os.chdir(path)
+# print(os.listdir(path))
+
+
+# creating a file
+
+# os.mkdir("example.py")
+# os.rmdir("example.py")
+
+# removing a file
+# os.remove("example.py")
+
+path = r"C:\Users\Vidyashree M C\PycharmProjects\Alpha4\files\sample.txt"
+
+os.popen(path)
+
+
+
+
+
+
+
