18f828114f30b8050eed5b2e63c279dc787f9736
vidyagowda159 <vidyagowda005@gmail.com>
2022-04-08 16:15:07 +0530

    regular expressions

--- basics.py
+++ basics.py
@@ -16,3 +16,30 @@
 
 # print(sep="@", "hai", "hello")  # SyntaxError
 
+
+s = "so123p56ltd78"
+i = 0
+while i < len(s):
+    j = num = []
+    z = 0
+    while i < len(s):
+        if s[i].isdigit():
+            j.append(s[i])
+            i += 1
+
+        else:
+            i += 1
+            break
+    print(j)
+
+
+
+
+
+
+
+
+
+
+
+
