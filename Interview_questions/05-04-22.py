a18bf64ecf0d0ec9c9555a8417992eeda45a0f38
vidyagowda159 <vidyagowda005@gmail.com>
2022-04-18 17:13:26 +0530

    solutions for interview questions

--- Interview_questions/05-04-22.py
+++ Interview_questions/05-04-22.py
@@ -1,6 +1,6 @@
 # comma separated string
 
-s = "hello welcome to python"
+# s = "hello welcome to python"
 
 # separating each character
 # list_ = list(s)
@@ -10,20 +10,23 @@ s = "hello welcome to python"
 # print(*words, sep=",")
 
 ###############################################################################################
-s = "hello welcome to python"
-res = ""
-for char in s:
-    if s.count(char) > 1:   # if char in res:
-        res = res + "-"
-    else:
-        res = res + char
+# s = "hello welcome to python"
+# res = ""
+# for char in s:
+#     if s.count(char) > 1:   # if char in res:
+#         res = res + "-"
+#     else:
+#         res = res + char
 
 
+# **126 In the list below, find all the number pairs which results in 10 either when added or subtracted**
+
+a = [3, 5, -4, 8, 11, 1, -1, 5, 6]
+res = []
+for i in range(len(a)):
+    for j in range(i+1, len(a)):
+        if abs(a[i]) + abs(a[j]) == 10 or a[i] - a[j] == 10:
+            res.append([a[i], a[j]])
 
 
-
-
-
-
-
-
+# print(res)
