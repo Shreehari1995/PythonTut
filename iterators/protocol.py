9e7f16d0d26337ddf1bd5ac02b2152be2556f6d9
shree <shreeharirgp@gmail.com>
2022-10-11 12:28:53 +0530

    alpha4

--- iterators/protocol.py
+++ iterators/protocol.py
@@ -17,32 +17,42 @@ print(a)
 # print(next(a))
 # print(next(a))
 
-for item in l:
-    print(item)
+# for item in l:
+#     print(item)
+#
+#
+# # iterator protocol
+#
+# a = iter(l)
+#
+# while True:
+#     try:
+#         item = next(a)
+#
+#     except StopIteration:
+#         break
+#
+#     print(item)
 
 
-# iterator protocol
-
-a = iter(l)
-
-while True:
-    try:
-        item = next(a)
-
-    except StopIteration:
-        break
-
-    print(item)
+# from sketchpy import library as lib
 
 
+# o = lib
+print(bin(10))
 
+l = [4, 6, 3, 5, 7]
+c = 0
+for i in l:
+    for j in l:
+        if i | j == i:
+            c += 1
+print(c)
 
 
 
 
-
-
-
+print(4 | 6)
 
 
 
